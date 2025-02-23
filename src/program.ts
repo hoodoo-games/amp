0f282f50da5dccca951d40c1183fe697b425a069
eswaidner <eswaidner2@gmail.com>
2025-02-23 13:42:20 -0700

    more program structure changes

--- src/program.ts
+++ src/program.ts
@@ -1,23 +1,12 @@
 // compile-time representation of program
-export class Program {
-  root: Node;
-  elements: Record<string, Element> = {};
-
-  constructor(name: string) {
-    this.root = {
-      name,
-      comments: [],
-      public: true,
-      export: false,
-      inputs: [],
-      outputs: [],
-      impl: { kind: "graph", graph: { nodes: [], edges: {} } },
-    };
-  }
+export interface Program {
+  root: string;
+  elements: Record<string, Element>;
 }
 
 interface Element {
   name: string;
+  public: boolean;
   comments: string[];
 }
 
@@ -31,26 +20,17 @@ type Type =
   | { kind: "sum"; def: Type[] }
   | { kind: "product"; def: Type[] };
 
+//TODO type comparisons (subset/superset)
+
 interface NamedType extends Element {
   fields: { name: string; type: Type }[];
 }
 
-interface Constant {
-  t: { kind: "numeric"; val: number } | { kind: "boolean"; val: boolean };
-  length: number;
-}
-
 export interface Node extends Element {
-  public: boolean;
   export: boolean;
   inputs: Port[];
   outputs: Port[];
-  impl: { kind: "import" } | { kind: "graph"; graph: NodeGraph };
-}
-
-interface NodeGraph {
-  nodes: string[];
-  edges: Record<string, string>; // "node/port": "node/port"
+  impl: NodeImpl;
 }
 
 interface Port {
@@ -58,17 +38,53 @@ interface Port {
   type: Type;
 }
 
+type NodeImpl =
+  | { kind: "core" }
+  | { kind: "import" }
+  | { kind: "graph"; graph: NodeGraph };
+
+interface NodeGraph {
+  nodes: string[];
+  links: Record<string, string>; // "node/port": "node/port"
+}
+
 export class ProgramBuilder {
-  name: string;
+  root: Node;
+  nodes: Record<string, Node> = {};
+  types: Record<string, NamedType> = {};
 
   constructor(name: string) {
-    this.name = name;
+    this.root = {
+      name,
+      public: true,
+      comments: [],
+      export: false,
+      inputs: [],
+      outputs: [],
+      impl: { kind: "graph", graph: { nodes: [], links: {} } },
+    };
   }
 
   build(): Program {
+    const p = { root: this.root.name, elements: {} };
+
     //TODO validate program
-    return new Program("app");
+    //TODO check for element name conflicts
+    //TODO check for invalid node impls
+    //TODO check for invalid links
+
+    return p;
   }
 
-  //TODO construction methods
+  node(n: Node): ProgramBuilder {
+    //TODO check for name conflict
+    this.nodes[n.name] = n;
+    return this;
+  }
+
+  type(t: NamedType): ProgramBuilder {
+    //TODO check for name conflict
+    this.types[t.name] = t;
+    return this;
+  }
 }
